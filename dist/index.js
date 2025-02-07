d4b90ff85c988a59ed10a09de8e343e132356e93
ubaydillah1 <ubaydillah1737@gmail.com>
2025-02-07 15:17:18 +0700

    Connect to vercel

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,20 +16,35 @@ const express_1 = __importDefault(require("express"));
 require("dotenv/config");
 const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
 const googleOAuthRoutes_1 = __importDefault(require("./routes/googleOAuthRoutes"));
+const cors_1 = __importDefault(require("cors"));
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 app.use(express_1.default.json());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,PATCH");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Referrer-Policy", "no-referrer-when-downgrade");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+app.use((0, cors_1.default)({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+}));
 app.use("/oauth", googleOAuthRoutes_1.default);
 app.use("/api/dashboard", dashboardRoutes_1.default);
+app.get("/prisma", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const allUsers = yield prisma.user.findMany();
+        res.json({
+            user: allUsers,
+        });
+    }
+    catch (err) {
+        const error = err;
+        res.json({
+            message: error.message,
+        });
+    }
+}));
 app.get("*", (req, res) => {
     res.json({
         Error: "Not Found",
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
 import "dotenv/config";
 import dashboardRouter from "./routes/dashboardRoutes";
-import { OAuth2Client } from "google-auth-library";
 import googleOuthRouter from "./routes/googleOAuthRoutes";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const app = express();
 
@@ -10,18 +12,32 @@ const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 
 app.use(express.json());
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,PATCH");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Referrer-Policy", "no-referrer-when-downgrade");
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use("/oauth", googleOuthRouter);
 app.use("/api/dashboard", dashboardRouter);
+
+app.get("/prisma", async (req, res) => {
+  try {
+    const allUsers = await prisma.user.findMany();
+
+    res.json({
+      user: allUsers,
+    });
+  } catch (err) {
+    const error = err as Error;
+    res.json({
+      message: error.message,
+    });
+  }
+});
 
 app.get("*", (req: Request, res: Response) => {
   res.json({
