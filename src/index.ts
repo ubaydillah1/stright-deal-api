ed9f7d5413de565a1fe743497aeb0d23eb3310cf
ubaydillah1 <ubaydillah1737@gmail.com>
2025-02-11 17:57:16 +0700

    set cors

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use(
       "https://straight-deal.webflow.io",
       "http://127.0.0.1:5500/",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
