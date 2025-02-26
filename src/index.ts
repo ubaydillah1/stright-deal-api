bdc0b0646fe259730cd37c119682affef4d15345
ubaydillah1 <ubaydillah1737@gmail.com>
2025-02-26 13:38:04 +0700

    add stright deal origin for cores

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "https://www.straightdeal.com",
       "https://testing-s-deal-vercel.vercel.app",
       "https://straight-deal.webflow.io",
       "http://127.0.0.1:5500",
