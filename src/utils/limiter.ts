cf1595d0543065c9ce97e680f4f83326fcb56b13
ubaydillah1 <ubaydillah1737@gmail.com>
2025-03-06 10:58:01 +0700

    increase limit per 15 minutes

--- src/utils/limiter.ts
+++ src/utils/limiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 2000,
   message: "Too many requests from this IP, please try again later",
 });
 
