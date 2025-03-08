55a5ab942fa62c0bff52e254d4e565655a1be5b9
ubaydillah1 <ubaydillah1737@gmail.com>
2025-03-08 11:00:01 +0700

    remove origin testing

--- src/index.ts
+++ src/index.ts
@@ -25,15 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://www.straightdeal.com",
-      "https://straightdeal.com",
-      "https://testing-s-deal-vercel.vercel.app",
-      "https://straight-deal.webflow.io",
-      "http://127.0.0.1:5500",
-      "http://localhost:5500",
-    ],
+    origin: ["https://www.straightdeal.com", "https://straightdeal.com"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
